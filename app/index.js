6110518681e6e84d8dc1c2b9ffb0f88d28278206
Jimmy Palafox <85587136+hazedCadet@users.noreply.github.com>
2023-07-27 12:04:32 -0400

    react intro

--- app/index.js
+++ app/index.js
@@ -1,1 +1,39 @@
 // Your code here!
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// const root = ReactDOM.createRoot(document.getElementById("app"));
+// root.render(<h1>Hello World</h1>);
+
+// const root = ReactDOM.createRoot(document.getElementById("app"));
+// root.render(<h1>Hello from React!</h1>);
+
+/* ADD BEFORE ROOT!!!! */
+// function Counter {
+//     return (
+//       <div id="container">
+//         <div id="navbar">Counter.js</div>
+//         <div id="counter">
+//           <h1>0</h1>
+//           <button>Increment</button>
+//         </div>
+//       </div>
+//     );
+//   }
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+
+function Counter {
+    return (
+        <div id="container">
+            <div id="navbar">Counter.js</div>
+            <div id="counter">
+                <h1>0</h1>
+                <button>Increment</button>
+            </div>
+        </div>
+    );
+}
+
+root.render(<Counter/>)
